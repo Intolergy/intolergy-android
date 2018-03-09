bc31b3142bf400b3eea0cb7bcab35aa70753c2aa
Brais Piñeiro <brapifra@gmail.com>
2018-03-09 20:26:25 +0100

    Bottom icon bar

--- app/src/main/java/com/intolergy/intolergy/MainActivity.java
+++ app/src/main/java/com/intolergy/intolergy/MainActivity.java
@@ -3,6 +3,8 @@ package com.intolergy.intolergy;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,27 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_main);
+        ImageView foodIcon=(ImageView)findViewById(R.id.foodIcon);
+        ImageView homeIcon=(ImageView)findViewById(R.id.homeIcon);
+        ImageView restaurantIcon = (ImageView)findViewById(R.id.restaurantIcon);
+        foodIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                System.out.println("FoodIcon");
+            }
+        });
+        homeIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                System.out.println("HomeIcon");
+            }
+        });
+        restaurantIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                System.out.println("RestaurantIcon");
+            }
+        });
     }
 
 }
