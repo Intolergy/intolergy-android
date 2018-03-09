f8e5ee02170d7f09c14e2f2b4d1a6f1528f85029
Maria Losada <mariamolgas@gmail.com>
2018-03-09 14:09:05 +0100

    login

--- app/src/main/java/com/intolergy/intolergy/MainActivity.java
+++ app/src/main/java/com/intolergy/intolergy/MainActivity.java
@@ -1,13 +1,61 @@
 package com.intolergy.intolergy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button= null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                login();
+            }
+        });
     }
+
+
+    public void forgot(View v) {
+        Log.d("login","contraseña olvidada");
+
+    }
+
+    public void register(View v) {
+        Log.d("login","contraseña olvidada");
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+
+    private void login(){
+
+        EditText username = (EditText)findViewById(R.id.user);
+        EditText password = (EditText)findViewById(R.id.pass);
+
+        if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+            Log.d("login","correcto");
+            //correcct password
+        }else{
+            Log.d("login","incorrecto");
+            //wrong password
+        }
+
+    }
+
 }
