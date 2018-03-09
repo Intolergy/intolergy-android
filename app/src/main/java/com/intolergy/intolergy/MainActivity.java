0cc3163141c097cb4ca8531bf028f8ff9d9ee801
Brais Piñeiro <brapifra@gmail.com>
2018-03-09 19:36:27 +0100

    Basic MainActivity

--- app/src/main/java/com/intolergy/intolergy/MainActivity.java
+++ app/src/main/java/com/intolergy/intolergy/MainActivity.java
@@ -1,6 +1,7 @@
 package com.intolergy.intolergy;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_main);
     }
 
